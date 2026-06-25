ad428d312c19872d261d27530f6853b89b7ef66f
Hector Vargas <hecto@MacBook-Pro.local>
2026-06-25 05:47:20 -0400

    [Feature] added the json and core features

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
-message = "Welcome to Task Tracker CLI"
+import json
 
+message = "Welcome to Task Tracker CLI"
 # add a task
 def add():
     user_input = input("Enter a task: ")
-    return user_input
+    user_description = input("Enter a description: ")
+    with open("list.json", "r") as f:
+        data = json.load(f)
+        data["tasks"].append(user_input)
+    with open("list.json", "w") as f:
+        json.dump(data, f)
+    return user_input, user_description
 
+def status():
+    pass
+    
 # update a task
 def update():
-    pass
+    task_id = input("Enter task ID to update: ")
+    new_task = input("Enter updated task: ")
+    return task_id, new_task
+
+def list_tasks():
+    with open("list.json", "r") as f:
+        data = json.load(f)
+        print(data)
 
 # delete a task
 def delete():
-    pass
+    tasks_id = input("Enter task ID to delete: ")
+    with open("list.json", "r") as f:
+        data = json.load(f)
+        data["tasks"] = [task for task in data["tasks"] if task != tasks_id]
+        with open("list.json", "w") as f:
+            json.dump(data, f)
    
 def main():
     print(message)
-    add()
+    print("1: Add task")
+    print("2: Update task")
+    print("3: List tasks")
+    print("4: Delete task")
+    
+    match input("Enter a command: "):
+        case "1":
+            add()
+        case "2":
+            update()
+        case "3":
+            list_tasks()
+        case "4":
+            delete()
+        case _:
+            print("Invalid command")
 
 main()
