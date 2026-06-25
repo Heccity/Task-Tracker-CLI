8d53bbf8218b356f9e4390c1ec93793f80c405b7
Hector Vargas <hecto@MacBook-Pro.local>
2026-06-25 00:28:57 -0400

    initial commit and added structure to app

---
+++ main.py
@@ -0,0 +1,20 @@
+message = "Welcome to Task Tracker CLI"
+
+# add a task
+def add():
+    user_input = input("Enter a task: ")
+    return user_input
+
+# update a task
+def update():
+    pass
+
+# delete a task
+def delete():
+    pass
+   
+def main():
+    print(message)
+    add()
+
+main()
